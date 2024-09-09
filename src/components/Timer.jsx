d1bb3ac354b47420b88562e07ff07faa7c21a276
BotS3k <sujeeth2004@gmail.com>
2024-09-09 23:10:56 +0530

    feat: quiz protal

---
+++ src/components/Timer.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Timer() {
+  const [seconds, setSeconds] = useState(300); // 5 minutes in seconds
+  const [isActive, setIsActive] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    let intervalId;
+    if (isActive) {
+      intervalId = setInterval(() => {
+        setSeconds(seconds - 1);
+      }, 1000);
+    }
+    return () => clearInterval(intervalId);
+  }, [isActive, seconds]);
+
+  useEffect(() => {
+    if (seconds === 0) {
+      setIsActive(false);
+      navigate("/lobby");
+    }
+  }, [seconds]);
+
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+
+  return (
+    <div>
+      <div className=" text-main_font bg-[#D9D9D940] px-4 py-2 -skew-x-[40deg] inline-block relative">
+        <div className="skew-x-[40deg]">
+          <h1>
+            Time Left: {minutes}:{remainingSeconds.toString().padStart(2, "0")}
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Timer;
